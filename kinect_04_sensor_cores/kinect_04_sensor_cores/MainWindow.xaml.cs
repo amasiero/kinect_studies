4b0ca3d2496633139bc3e047b748c6d09d2e3a7d
Andrey Masiero <andreymasiero@gmail.com>
2016-05-12 13:53:20 -0300

    Motor Shaft and Sensor RGB project added

---
+++ kinect_04_sensor_cores/kinect_04_sensor_cores/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+using AuxiliarKinect.FuncoesBasicas;
+
+namespace kinect_04_sensor_cores
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        KinectSensor kinect;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            kinect = InicializadorKinect.InicializarPrimeiroSensor(10);
+            kinect.ColorStream.Enable();
+        }
+
+        private BitmapSource ObterImagemSensorRGB(ColorImageFrame quadro)
+        {
+            using(quadro)
+            {
+                byte[] bytesImagem = new byte[quadro.PixelDataLength];
+                quadro.CopyPixelDataTo(bytesImagem);
+
+                return BitmapSource.Create(quadro.Width, quadro.Height, 96, 96, PixelFormats.Bgr32, null, bytesImagem, quadro.Width * quadro.BytesPerPixel);
+            }
+        }
+
+        private void Button_BaterFoto(object sender, EventArgs e)
+        {
+            imagemCamera.Source = ObterImagemSensorRGB(kinect.ColorStream.OpenNextFrame(0));
+        }
+    }
+}
