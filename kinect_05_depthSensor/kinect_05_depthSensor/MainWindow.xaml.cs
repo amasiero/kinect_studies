72ceb3d15d92128d92c94fc48db4134e91fe446b
Andrey Masiero <andreymasiero@gmail.com>
2016-05-16 14:39:11 -0300

    Depth Sensor Projects added

---
+++ kinect_05_depthSensor/kinect_05_depthSensor/MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+using AuxiliarKinect.FuncoesBasicas;
+
+namespace kinect_05_depthSensor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        KinectSensor kinect;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InicializarSeletor();
+        }
+
+        private void InicializarSeletor()
+        {
+            InicializadorKinect inicializador = new InicializadorKinect();
+            inicializador.MetodoInicializadorKinect = InicializarKinect;
+            seletorSensorUI.KinectSensorChooser = inicializador.SeletorKinect;
+        }
+
+        private void InicializarKinect(KinectSensor kinectSensor)
+        {
+            kinect = kinectSensor;
+            slider.Value = kinect.ElevationAngle;
+
+            kinect.DepthStream.Enable();
+            kinect.DepthFrameReady += kinect_DepthFrameReady;
+            kinect.DepthStream.Range = DepthRange.Near;
+            kinect.SkeletonStream.Enable();
+            
+        }
+
+        private BitmapSource ObterImagemSensorRGB(ColorImageFrame quadro)
+        {
+            if (quadro == null) return null;
+
+            using (quadro)
+            {
+                byte[] bytesImagem = new byte[quadro.PixelDataLength];
+                quadro.CopyPixelDataTo(bytesImagem);
+
+                if (chkEscalaCinza.IsChecked.HasValue && chkEscalaCinza.IsChecked.Value)
+                    for (int indice = 0; indice < bytesImagem.Length; indice += quadro.BytesPerPixel)
+                    {
+                        byte maiorValorCor = Math.Max(bytesImagem[indice], Math.Max(bytesImagem[indice + 1], bytesImagem[indice + 2]));
+                        bytesImagem[indice] = maiorValorCor;
+                        bytesImagem[indice + 1] = maiorValorCor;
+                        bytesImagem[indice + 2] = maiorValorCor;
+                    }
+
+                return BitmapSource.Create(quadro.Width, quadro.Height, 96, 96, PixelFormats.Bgr32, null, bytesImagem, quadro.Width * quadro.BytesPerPixel);
+            }
+
+        }
+
+        private BitmapSource ReconhecerHumanos(DepthImageFrame quadro)
+        {
+            if (quadro == null) return null;
+
+            using (quadro)
+            {
+                DepthImagePixel[] imagemProfundidade = new DepthImagePixel[quadro.PixelDataLength];
+
+                quadro.CopyDepthImagePixelDataTo(imagemProfundidade);
+
+                byte[] bytesImagem = new byte[imagemProfundidade.Length * 4];
+
+                for (int indice = 0; indice <bytesImagem.Length; indice += 4)
+                {
+                    if (imagemProfundidade[indice / 4].PlayerIndex != 0)
+                    {
+                        bytesImagem[indice + 1] = 255;
+                    }
+                        
+                }
+           
+                return BitmapSource.Create(quadro.Width, quadro.Height, 96, 96, PixelFormats.Bgr32, null, bytesImagem, quadro.Width * 4);
+            }
+
+        }
+
+        private void kinect_ColorFrameReady(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            imagemCamera.Source = ObterImagemSensorRGB(e.OpenColorImageFrame());
+        }
+
+        private void kinect_DepthFrameReady(object sender, DepthImageFrameReadyEventArgs e)
+        {
+            imagemCamera.Source = ReconhecerHumanos(e.OpenDepthImageFrame());
+        }
+
+        private void slider_DragCompleted(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
+        {
+            kinect.ElevationAngle = Convert.ToInt32(slider.Value);
+        }
+    }
+}
