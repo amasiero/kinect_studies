7a6f30bd0b61deb1ede8f28115a6883cf1f462e1
Andrey Masiero <andreymasiero@gmail.com>
2016-05-17 21:30:05 -0300

    Skeleton´s Projects Added

---
+++ kinect_06_skeleton_01/kinect_06_skeleton_01/EsqueletoUsuarioAuxiliar.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Kinect;
+using System.Windows.Shapes;
+using System.Windows.Media;
+using System.Windows.Controls;
+
+namespace kinect_06_skeleton_01
+{
+    class EsqueletoUsuarioAuxiliar
+    {
+        private KinectSensor kinect;
+
+        public EsqueletoUsuarioAuxiliar(KinectSensor kinect)
+        {
+            this.kinect = kinect;
+        }
+
+        private ColorImagePoint ConverterCoordenadasArticulacao (Joint articulacao, double larguraCanvas, double alturaCanvas)
+        {
+            ColorImagePoint posicaoArticulacao = kinect.CoordinateMapper.MapSkeletonPointToColorPoint(articulacao.Position, kinect.ColorStream.Format);
+
+            posicaoArticulacao.X = (int)(posicaoArticulacao.X * larguraCanvas) / kinect.ColorStream.FrameWidth;
+
+            posicaoArticulacao.Y = (int)(posicaoArticulacao.Y * alturaCanvas) / kinect.ColorStream.FrameHeight;
+
+            return posicaoArticulacao;
+        }
+
+        private Ellipse CriarComponenteVisualArticulacao (int diametroArticulacao, int larguraDesenho, Brush corDesenho)
+        {
+            Ellipse objetoArticulacao = new Ellipse();
+
+            objetoArticulacao.Height = diametroArticulacao;
+            objetoArticulacao.Width = diametroArticulacao;
+            objetoArticulacao.StrokeThickness = larguraDesenho;
+            objetoArticulacao.Stroke = corDesenho;
+
+            return objetoArticulacao;
+        }
+
+        public void DesenharArticulacao(Joint articulacao, Canvas canvasParaDesenhar)
+        {
+            int diametroArticulacao = articulacao.JointType == JointType.Head ? 25 : 10;
+
+            int larguraDesenho = 4;
+
+            Brush corDesenho = Brushes.Red;
+
+            Ellipse objetoArticulacao = CriarComponenteVisualArticulacao(diametroArticulacao, larguraDesenho, corDesenho);
+
+            ColorImagePoint posicaoArticulacao = ConverterCoordenadasArticulacao(articulacao, canvasParaDesenhar.ActualWidth, canvasParaDesenhar.ActualHeight);
+
+            double deslocamentoHorizontal = posicaoArticulacao.X - objetoArticulacao.Width / 2;
+            double deslocamnetoVertical = posicaoArticulacao.Y - objetoArticulacao.Height / 2;
+
+            if ( deslocamnetoVertical >= 0 &&
+                 deslocamnetoVertical < canvasParaDesenhar.ActualHeight &&
+                 deslocamentoHorizontal >= 0 &&
+                 deslocamentoHorizontal < canvasParaDesenhar.ActualWidth )
+            {
+                Canvas.SetLeft(objetoArticulacao, deslocamentoHorizontal);
+                Canvas.SetTop(objetoArticulacao, deslocamnetoVertical);
+                Canvas.SetZIndex(objetoArticulacao, 100);
+
+                canvasParaDesenhar.Children.Add(objetoArticulacao);
+            }
+        }
+
+        private Line CriarComponenteVisualOsso (int larguraDesenho, Brush corDesenho, double origemX, double origemY, double destinoX, double destinoY)
+        {
+            Line objetoOsso = new Line();
+
+            objetoOsso.StrokeThickness = larguraDesenho;
+            objetoOsso.Stroke = corDesenho;
+
+            objetoOsso.X1 = origemX;
+            objetoOsso.Y1 = origemY;
+
+            objetoOsso.X2 = destinoX;
+            objetoOsso.Y2 = destinoY;
+
+            return objetoOsso;
+        }
+
+        public void DesenharOsso(Joint articulacaoOrigem, Joint articulacaoDestino, Canvas canvasParaDesenhar)
+        {
+            int larguraDesenho = 4;
+            Brush corDesenho = Brushes.Green;
+
+            ColorImagePoint posicaoArticulacaoOrigem = ConverterCoordenadasArticulacao(articulacaoOrigem, canvasParaDesenhar.ActualWidth, canvasParaDesenhar.ActualHeight);
+            ColorImagePoint posicaoArticulacaoDestino = ConverterCoordenadasArticulacao(articulacaoDestino, canvasParaDesenhar.ActualWidth, canvasParaDesenhar.ActualHeight);
+
+            Line objetoOsso = CriarComponenteVisualOsso(larguraDesenho, corDesenho, posicaoArticulacaoOrigem.X, posicaoArticulacaoOrigem.Y, posicaoArticulacaoDestino.X, posicaoArticulacaoDestino.Y);
+
+            if( Math.Max(objetoOsso.X1, objetoOsso.X2) < canvasParaDesenhar.ActualWidth &&
+                Math.Min(objetoOsso.X1, objetoOsso.X2) > 0 &&
+                Math.Max(objetoOsso.Y1, objetoOsso.Y2) < canvasParaDesenhar.ActualHeight &&
+                Math.Min(objetoOsso.Y1, objetoOsso.Y2) > 0)
+            {
+                canvasParaDesenhar.Children.Add(objetoOsso);
+            }
+        }
+    }
+}
---
+++ kinect_06_skeleton_04/kinect_06_skeleton_04/MainWindow.xaml.cs
@@ -0,0 +1,141 @@
+﻿using Auxiliar;
+using AuxiliarKinect.FuncoesBasicas;
+using Microsoft.Kinect;
+using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace kinect_06_skeleton_04
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        KinectSensor kinect;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InicializarSeletor();
+        }
+
+        private void InicializarSeletor()
+        {
+            InicializadorKinect inicializador = new InicializadorKinect();
+            inicializador.MetodoInicializadorKinect = InicializarKinect;
+            seletorSensorUI.KinectSensorChooser = inicializador.SeletorKinect;
+        }
+
+        private void InicializarKinect(KinectSensor kinectSensor)
+        {
+            kinect = kinectSensor;
+            slider.Value = kinect.ElevationAngle;
+
+            kinect.DepthStream.Enable();
+            kinect.SkeletonStream.Enable();
+            kinect.ColorStream.Enable();
+            kinect.AllFramesReady += kinect_AllFramesReady;
+        }
+
+        private void kinect_AllFramesReady(object sender, AllFramesReadyEventArgs e)
+        {
+            byte[] imagem = ObterImagemSensorRGB(e.OpenColorImageFrame());
+
+            if (chkEscalaCinza.IsChecked.HasValue && chkEscalaCinza.IsChecked.Value)
+                ReconhecerDistancia(e.OpenDepthImageFrame(), imagem, 2000);
+
+            if (imagem != null)
+                canvasKinect.Background = new ImageBrush(BitmapSource.Create(kinect.ColorStream.FrameWidth, kinect.ColorStream.FrameHeight, 96, 96, PixelFormats.Bgr32, null, imagem, kinect.ColorStream.FrameBytesPerPixel * kinect.ColorStream.FrameWidth));
+
+            canvasKinect.Children.Clear();
+            if (chkEsqueleto.IsChecked.HasValue && chkEsqueleto.IsChecked.Value)
+                DesenharEsqueletoUsuario(e.OpenSkeletonFrame());
+        }
+
+        private void ReconhecerDistancia(DepthImageFrame quadro, byte[] bytesImagem, int distanciaMaxima)
+        {
+            if (quadro == null || bytesImagem == null) return;
+
+            using (quadro)
+            {
+                DepthImagePixel[] imagemProfundidade = new DepthImagePixel[quadro.PixelDataLength];
+                quadro.CopyDepthImagePixelDataTo(imagemProfundidade);
+
+                DepthImagePoint[] pontosImagemProfundidade = new DepthImagePoint[640 * 480];
+
+                kinect.CoordinateMapper.MapColorFrameToDepthFrame(kinect.ColorStream.Format, kinect.DepthStream.Format, imagemProfundidade, pontosImagemProfundidade);
+
+                for (int indice = 0; indice < pontosImagemProfundidade.Length; indice++)
+                {
+                    var ponto = pontosImagemProfundidade[indice];
+                    if (ponto.Depth < distanciaMaxima && KinectSensor.IsKnownPoint(ponto))
+                    {
+                        int indiceImageCores = indice * 4;
+                        byte maiorValorCor = Math.Max(bytesImagem[indiceImageCores], Math.Max(bytesImagem[indiceImageCores + 1], bytesImagem[indiceImageCores + 2]));
+
+                        bytesImagem[indiceImageCores] = maiorValorCor;
+                        bytesImagem[indiceImageCores + 1] = maiorValorCor;
+                        bytesImagem[indiceImageCores + 2] = maiorValorCor;
+                    }
+                }
+            }
+        }
+
+        private void DesenharEsqueletoUsuario(SkeletonFrame quadro)
+        {
+            if (quadro == null) return;
+
+            using (quadro)
+            {
+                quadro.DesenharEsqueletoUsuario(kinect, canvasKinect);
+            }
+        }
+
+        private byte[] ObterImagemSensorRGB(ColorImageFrame quadro)
+        {
+            if (quadro == null) return null;
+
+            using (quadro)
+            {
+                byte[] bytesImagem = new byte[quadro.PixelDataLength];
+                quadro.CopyPixelDataTo(bytesImagem);
+
+                return bytesImagem;
+            }
+
+        }
+
+        private BitmapSource ReconhecerHumanos(DepthImageFrame quadro)
+        {
+            if (quadro == null) return null;
+
+            using (quadro)
+            {
+                DepthImagePixel[] imagemProfundidade = new DepthImagePixel[quadro.PixelDataLength];
+
+                quadro.CopyDepthImagePixelDataTo(imagemProfundidade);
+
+                byte[] bytesImagem = new byte[imagemProfundidade.Length * 4];
+
+                for (int indice = 0; indice < bytesImagem.Length; indice += 4)
+                {
+                    if (imagemProfundidade[indice / 4].PlayerIndex != 0)
+                    {
+                        bytesImagem[indice + 1] = 255;
+                    }
+
+                }
+
+                return BitmapSource.Create(quadro.Width, quadro.Height, 96, 96, PixelFormats.Bgr32, null, bytesImagem, quadro.Width * 4);
+            }
+
+        }
+
+        private void slider_DragCompleted(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
+        {
+            kinect.ElevationAngle = Convert.ToInt32(slider.Value);
+        }
+    }
+}
